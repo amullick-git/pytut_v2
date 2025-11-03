a460f17659dbd108d6e7355a9211f76c0a6ea737
Amarnath Mullick <amullick@google.com>
2025-11-03 15:10:16 -0800

    Added a placeholder for next chapters

---
+++ chapters/chapter4.js
@@ -0,0 +1,19 @@
+console.log("Loading chapter4.js..."); // eslint-disable-line no-console
+
+export const chapter4 = {
+    title: "Coming Soon!",
+    story: `
+        <div class="alert alert-success">
+            <h4 class="alert-heading">Great work!</h4>
+            <p>
+                You've completed all the available lessons for now. More chapters are on the way!
+            </p>
+            <hr>
+            <p class="mb-0">
+                Check back soon for new adventures in Python.
+            </p>
+        </div>
+    `,
+    sections: [],
+    exercises: []
+};
--- tutorial.js
+++ tutorial.js
@@ -1,11 +1,13 @@
 import { chapter1 } from './chapters/chapter1.js';
 import { chapter2 } from './chapters/chapter2.js';
 import { chapter3 } from './chapters/chapter3.js';
+import { chapter4 } from './chapters/chapter4.js';
 
 const tutorialContent = {
     chapter1,
     chapter2,
-    chapter3
+    chapter3,
+    chapter4
 };
 
 window.tutorialContent = tutorialContent; // Make it globally available
